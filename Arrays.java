5b7f0dfc8509d844fc3613255f77792163c0f666
Saurabhkumar Marpadge <saurabhmarpadge29@gmail.com>
2019-09-17 22:33:48 +0530

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+/*
+-size() - number of items
+-capacity() - number of items it can hold
+-is_empty()
+-at(index) - returns item at given index, blows up if index out of bounds
+-push(item)
+-insert(index, item) - inserts item at index, shifts that index's value and trailing elements to the right
+-prepend(item) - can use insert above at index 0
+-pop() - remove from end, return value
+-delete(index) - delete item at index, shifting all trailing elements left
+-remove(item) - looks for value and removes index holding it (even if in multiple places)
+-find(item) - looks for value and returns first index with that value, -1 if not found
+-resize(new_capacity) // private function
+    when you reach capacity, resize to double the size
+    when popping an item, if size is 1/4 of capacity, resize to half
+*/
+
+public class Arrays {
+  public static void main(String[] args){
+  
+  }
+}
