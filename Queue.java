81a5975c3b374c81dd33dd7c8a8eceb4dc37d792
Saurabhkumar Marpadge <saurabhmarpadge29@gmail.com>
2019-09-17 22:38:14 +0530

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,17 @@
+/*
+Implement using linked-list, with tail pointer:
+  enqueue(value) - adds value at position at tail
+  dequeue() - returns value and removes least recently added element (front)
+  empty()
+Implement using fixed-sized array:
+  enqueue(value) - adds item at end of available storage
+  dequeue() - returns value and removes least recently added element
+  empty()
+  full()
+*/
+
+public class Queue {
+  public static void main(String[] args){
+  
+  }
+}
