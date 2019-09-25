1f7362937653acd32fbfd42e9fece3ae03c842e1
Saurabhkumar Marpadge <saurabhmarpadge29@gmail.com>
2019-09-25 16:12:01 +0000

    Updating merge sort

--- Algorithm/Sorting/MergeSort.java
+++ Algorithm/Sorting/MergeSort.java
@@ -1,39 +1,53 @@
+package com.smallerelement;
+
+import java.io.*;
 import java.util.*;
 
-public class MergeSort{
-    public static void main(String[] args){
-
+public class MergeSort {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while(tc-->0) {
+            int size = Integer.parseInt(br.readLine());
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            mergeSort(input);
+            Arrays.stream(input).forEach(System.out::print);
+            System.out.println();
+        }
     }
 
-    public void mergeSort(ArrayList<Integer> list, int low, int high){
-        int mid = low + (high-low)/2;
-        mergeSort(list,low,mid);
-        mergeSort(list,mid+1,high);
-        merge(list,low,high);
+    private static void mergeSort(int[] input) {
+        mergeSort(input, 0, input.length-1);
     }
 
-    public void merge(ArrayList<Integer> list, int low, int high){
-        int mid = low+(high-low)/2;
-        ArrayList<Integer> temp = new ArrayList<>();
-        int secondStart = mid+1;
-        int firstStart = low;
-        while(firstStart<=mid&&secondStart<=high){
-            if(list.get(firstStart)<=list.get(secondStart)){
-                temp.add(list.get(firstStart));
-                firstStart++;
+    public static void mergeSort(int[] lists, int low, int high){
+        if(low==high){
+            return;
+        }
+        int mid = low + ((high-low)>>1);
+        mergeSort(lists,low,mid);
+        mergeSort(lists,mid+1,high);
+        merge(lists,low,high,mid);
+    }
+
+    private static void merge(int[] lists, int low, int high, int mid) {
+        int p1 = low;
+        int p2 = mid+1;
+        int[] temp = new int[high-low+1];
+        int tIdx = 0;
+        while(p1<=mid&&p2<=high){
+            if(lists[p1]<=lists[p2]){
+                temp[tIdx++] = lists[p1++];
             } else {
-                temp.add(list.get(secondStart));
-                secondStart++;
+                temp[tIdx++] = lists[p2++];
             }
         }
-        while(firstStart<=mid){
-            temp.add(list.get(firstStart));
-            firstStart++;
+        while(p1<=mid){
+            temp[tIdx++] = lists[p1++];
         }
-        while(secondStart<=high){
-            temp.add(list.get(secondStart));
-            secondStart++;
+        while(p2<=high){
+            temp[tIdx++] = lists[p2++];
         }
-        System.arraycopy(temp,0,list,low,high-low+1);
+        System.arraycopy(temp,0,lists,low,high-low+1);
     }
 }
