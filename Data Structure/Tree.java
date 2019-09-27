98926fd52d7f221ae7212ef1a7bbe256f21a1c84
Saurabhkumar Marpadge <saurabhmarpadge29@gmail.com>
2019-09-27 05:09:36 +0000

    Adding tree traversal

--- Data Structure/Tree.java
+++ Data Structure/Tree.java
@@ -16,7 +16,53 @@ BFS(breadth-first search) and DFS(depth-first search) (video)
 */
 
 public class Tree {
-  public static void main(String[] args){
 
+  public static void main(String[] args){
+      Node c = new Node(null,3,null);
+      Node d = new Node(null,4,null);
+      Node f = new Node(null,5,null);
+      Node b = new Node(d,2,f);
+      Node a = new Node(b,1,c);
+
+      inOrder(a);
+  }
+
+  public static void inOrder(Node node){
+      if(node==null){
+          return;
+      }
+      inOrder(node.left);
+      System.out.println(node.item);
+      inOrder(node.right);
+  }
+
+  public static void preOrder(Node node){
+      if(node==null){
+          return;
+      }
+
+      System.out.println(node.item);
+      preOrder(node.left);
+      preOrder(node.right);
+  }
+
+  public static void postOrder(Node node){
+      if(node==null){
+          return;
+      }
+
+      postOrder(node.left);
+      postOrder(node.right);
+      System.out.println(node.item);
   }
 }
+    class Node {
+        int item;
+        Node left;
+        Node right;
+        Node(Node left,int item,Node right){
+            this.item = item;
+            this.left = left;
+            this.right = right;
+        }
+    }
